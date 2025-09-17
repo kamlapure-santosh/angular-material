5a36c1508a81a269adc5d7d5b044486fa8c9f617
Santosh Kamlapure <adm-SantKa96@csoups.com>
2025-09-17 17:36:40 +0530

    Responsive Design With Angular Material

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import { Course } from '../model/course';
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { filter } from 'rxjs/operators';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
     selector: 'courses-card-list',
@@ -14,23 +15,52 @@ export class CoursesCardListComponent implements OnInit {
 
     @Input()
     courses: Course[];
-
-    constructor(private dialog: MatDialog) {
-
+    cols = 3;
+    rowHeight = '500px';
+    handsetPortrait = false;
+    constructor(private dialog: MatDialog, private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
+  this.responsive.observe([
+            Breakpoints.TabletPortrait,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape
+        ])
+            .subscribe(result => {
 
+                this.cols = 3;
+                this.rowHeight = "500px";
+                this.handsetPortrait = false;
+
+                const breakpoints = result.breakpoints;
+
+                if (breakpoints[Breakpoints.TabletPortrait]) {
+                    this.cols = 1;
+                }
+                else if (breakpoints[Breakpoints.HandsetPortrait]) {
+                    this.cols = 1;
+                    this.rowHeight = "430px";
+                    this.handsetPortrait = true;
+                }
+                else if (breakpoints[Breakpoints.HandsetLandscape]) {
+                    this.cols = 1;
+                }
+                else if (breakpoints[Breakpoints.TabletLandscape]) {
+                    this.cols = 2;
+                }
+            });
     }
 
     editCourse(course: Course) {
         openEditCourseDialog(this.dialog, course)
-        .pipe(
-            filter(result => !!result)
-        )
-        .subscribe(
-            val => console.log("new course value", val)
-        );
+            .pipe(
+                filter(result => !!result)
+            )
+            .subscribe(
+                val => console.log("new course value", val)
+            );
     }
 }
 
